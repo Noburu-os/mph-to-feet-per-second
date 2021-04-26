edbe1a067060c893100f106f825ab056ac934952
Noburu-os <Jusin.driskell@outlook.com>
2021-04-26 13:29:24 -0500

    Add files via upload

---
+++ mph.py
@@ -0,0 +1,55 @@
+print('# This code computes feet per second to mph')
+import time
+
+# feel free to change the feet
+none = False
+miles = 60
+# dont change the sconds you wont get the correct value
+seconds_formula = 1.467
+print
+print
+print 'An object moving ' + str(miles) + ' miles per hour ' + str('(') + str(miles * seconds_formula) + ' feet per second' + str(')')
+#### ERROR THROWER ###############
+print('#') 
+print('#')
+if miles is not miles or seconds_formula is not seconds_formula:
+    print('ERROR: DEV')
+    print('this ERROR happens rarely try reloding the page')
+### this computes the time it takes to run the code
+time = time
+if time == time:
+    print('############ THIS IS THE TIME IT TAKES TO RUN THE CODE #############')
+    print time.time()
+    print('####################################################################')
+    if time == none:
+        print('time ERROR')
+        if time == unknown:
+            print('time ERROR') 
+
+print('#')
+print('#')
+print('#')
+print('#')
+print('################### THIS CONVERTS FEET PER SECOND TO MPH ######################')
+
+mph = 88.02
+
+one_second = 1.467
+
+if mph == mph and one_second == one_second:
+    print('An object moving ' + str('(') + str(mph) + ' feet per second' + str(')') +  
+    ' is moving' + str('(') + str(mph/one_second) + str(')') + 'miles per hour')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
